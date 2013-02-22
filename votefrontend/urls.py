578cc0c4ceadcb1f3d2ea8532d5181941142cc60
Lukas Martini <hello@lutoma.org>
2013-02-22 01:36:42 +0100

    votefrontend: Show questions; Allow voting on questions;

--- votefrontend/urls.py
+++ votefrontend/urls.py
@@ -7,4 +7,7 @@ The URL parts after /vote/. For the rest, see aovote/urls.py
 urlpatterns = patterns('',
 	url(r'^ident/(?P<token>[^\/]+)/$', 'votefrontend.views.ident', name='ident'),
 	url(r'^welcome/$', 'votefrontend.views.welcome', name='welcome'),
+	url(r'^question/(?P<question_id>[0-9]+)/$', 'votefrontend.views.show_question', name='show_question'),
+	url(r'^question/(?P<question_id>[0-9]+)/vote/$', 'votefrontend.views.submit_vote', name='submit_vote'),
+	url(r'^thanks/$', 'votefrontend.views.thanks', name='thanks'),
 )
--- votefrontend/views.py
+++ votefrontend/views.py
@@ -1,11 +1,12 @@
 # coding: utf-8
 
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template.context import RequestContext
 from django.core.urlresolvers import reverse
 from django.core.exceptions import PermissionDenied
-from vote.models import User
+from vote.models import User, PollQuestion, Vote
+from django.views.decorators.http import require_POST, require_safe
 
 def check_ident(function):
 	"""
@@ -13,22 +14,23 @@ def check_ident(function):
 	if the token was found
 	"""
 
-	def wrapper(*args):
+	def wrapper(*args, **keyword_args):
 		try:
 			token = args[0].COOKIES['oa_token']
 			function.__globals__['user'] = User.objects.get(token = token)
-
 		except (KeyError, User.DoesNotExist):
 			raise PermissionDenied
 
-		if not user.token == token:
+		# Double check just for the sake of it
+		if not function.__globals__['user'].token == token:
 			raise PermissionDenied
 
-		return function(*args)
+		return function(*args, **keyword_args)
 
 	wrapper.__doc__= function.__doc__
 	return wrapper
 
+@require_safe
 def ident(request, token):
 	"""
 	Stores the supplied token in a cookie so it doesn't have to be passed on in
@@ -39,6 +41,7 @@ def ident(request, token):
 	response.set_cookie('oa_token', token)
 	return response
 
+@require_safe
 @check_ident
 def welcome(request):
 	"""
@@ -46,7 +49,82 @@ def welcome(request):
 	their token
 	"""
 
-	response = render_to_response('votefrontend/welcome.html',
-		context_instance = RequestContext(request, {'user': user}))
+	first_question = PollQuestion.objects.filter(poll = user.poll).order_by('id')[0]
+	context = {'token_user': user, 'question': first_question}
 
+	response = render_to_response('votefrontend/welcome.html',
+		context_instance = RequestContext(request, context))
+
+	return response
+
+@require_safe
+@check_ident
+def show_question(request, question_id):
+	"""
+	Displays a single question to vote upon
+	"""
+
+	question = get_object_or_404(PollQuestion,
+		poll = user.poll,
+		id = question_id)
+
+	# Check if the user already voted on this (To show info message in template)
+	try:
+		vote = Vote.objects.get(voting_user = user, poll_question = question)
+	except Vote.DoesNotExist:
+		vote = None
+
+	context = {'user': user, 'question': question, 'vote': vote}
+	response = render_to_response('votefrontend/question.html',
+		context_instance = RequestContext(request, context))
+
+	return response
+
+@require_POST
+@check_ident
+def submit_vote(request, question_id):
+	"""
+	Submits a vote that has been made in the show_question view
+	"""
+
+	question = get_object_or_404(PollQuestion,
+		poll = user.poll,
+		id = question_id)
+
+	valid_choices = {'yes': 0, 'no': 1, 'abstention': 2}
+
+	# Check if the vote key exists and contains a valid value
+	if not 'vote' in request.POST or not request.POST['vote'] in valid_choices:
+		HttpResponseRedirect(reverse('show_question', kwargs = {'question_id': question.id}))
+
+	# Check if user already voted on this. If not, create new Vote object
+	try:
+		vote = Vote.objects.get(voting_user = user, poll_question = question)
+	except Vote.DoesNotExist:
+		vote = Vote(voting_user = user, poll_question = question)
+
+	vote.choice = valid_choices[request.POST['vote']]
+	vote.save()
+
+	next_question = question.next()
+	
+	if next_question == None:
+		return HttpResponseRedirect(reverse('thanks'))
+
+	return HttpResponseRedirect(reverse('show_question', kwargs = {'question_id': next_question.id}))
+
+@require_safe
+@check_ident
+def thanks(request):
+	"""
+	The page that's displayed after a user finished voting
+	"""
+
+	context = {'token_user': user}
+
+	response = render_to_response('votefrontend/thanks.html',
+		context_instance = RequestContext(request, context))
+
+	# Delete authorization cookie
+	response.delete_cookie('oa_token')
 	return response
