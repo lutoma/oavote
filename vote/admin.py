59742058c45f8d971ff5566b94fb015f740a0e25
Lukas Martini <hello@lutoma.org>
2013-04-09 01:34:28 +0200

    Prettier display of vote model

--- vote/admin.py
+++ vote/admin.py
@@ -4,7 +4,19 @@ from django.utils.translation import ugettext_lazy as _
 from vote.models import User, Poll, PollQuestion, Vote
 from django.contrib import admin
 
+class PollAdmin(admin.ModelAdmin):
+	list_display = ('title', 'active')
+	list_filter = ['active']
+
+class PollQuestionAdmin(admin.ModelAdmin):
+	list_display = ('title', 'authors', 'get_result')
+	list_filter = ['poll']
+
+class VoteAdmin(admin.ModelAdmin):
+	list_display = ('voting_user', 'poll_question', 'choice')
+	list_filter = ['poll_question']
+
 admin.site.register(User)
-admin.site.register(Poll)
-admin.site.register(PollQuestion)
-admin.site.register(Vote)
+admin.site.register(Poll, PollAdmin)
+admin.site.register(PollQuestion, PollQuestionAdmin)
+admin.site.register(Vote, VoteAdmin)
